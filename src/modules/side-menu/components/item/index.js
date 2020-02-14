61acca0c4be1404a8da8a92e6085b67f0fa07005
Kolecha <lskolecha@gmail.com>
2020-02-14 16:44:05 -0300

    Correção ng-if nas categorias do menu

--- src/modules/side-menu/components/item/index.js
+++ src/modules/side-menu/components/item/index.js
@@ -16,10 +16,11 @@ const item = {
 		}
 
 		vm.showSubCategory = () => {
-			if (vm.config && vm.config.children) {
-				return vm.config.children.filter((children) => !children.key || (vm.keys || []).indexOf(children.key) !== -1).length > 0
-			}
 			return true
+			// if (vm.config && vm.config.children) {
+			// 	return vm.config.children.filter((children) => !children.key || (vm.keys || []).indexOf(children.key) !== -1).length > 0
+			// }
+			// return true
 		}
 
 	}]
